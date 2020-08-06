1fd0f114c37b5577ddc0cceaac342fec095f55c7
s1270132 <s1270132@u-aizu.ac.jp>
2020-08-06 23:18:11 +0900

    HoT:Initial revision

---
+++ tosscoin.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class HoT{
+public static void main(String[] args){
+    int head=0,tail=0;
+    Random r = new Random();
+    for(int i=0;i<3;i++){
+        int coin=r.nextInt(2);
+        if(coin==0){
+        System.out.println("Round " +(i+1)+":"+ " Head");
+        head++;
+        }
+        if(coin==1){
+            System.out.println("Round "+(i+1)+" Tail");
+            tail++;
+        }
+    }
+    System.out.println("Heads: "+head+", Tails: "+tail);
+}
+}
